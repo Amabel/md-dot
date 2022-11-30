d3684fc94c80e87ad771189f43cadeffe77040e1
Amabel <luoweibinb@gmail.com>
2022-11-30 18:21:15 +0900

    feat: add showdown component

---
+++ src/components/Editor.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Editor</div>
+  <textarea name="textarea" id="" cols="30" rows="10"></textarea>
+  <VueShowdown markdown="## markdown text" flavor="github" :options="{ emoji: true }" />
+</template>
+
+<script setup lang="ts">
+import { VueShowdown } from 'vue-showdown'
+</script>
+
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { VueShowdownPlugin } from 'vue-showdown'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueShowdownPlugin, {
+  // 设置 showdown 默认 flavor
+  flavor: 'github',
+  // 设置 showdown 默认 options （会覆盖上面 flavor 的 options）
+  options: {
+    emoji: false,
+  },
+})
+
+app.mount('#app')
