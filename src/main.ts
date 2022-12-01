16b4aa529597ff11df4cd5f03b4b86b37cc2d339
Amabel <luoweibinb@gmail.com>
2022-12-01 14:58:53 +0900

    chore: config pinia

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.scss'
 import '../node_modules/github-markdown-css/github-markdown.css'
 import App from './App.vue'
 import { VueShowdownPlugin } from 'vue-showdown'
 
+const pinia = createPinia()
 const app = createApp(App)
-app.use(VueShowdownPlugin, {
-  // 设置 showdown 默认 flavor
-  flavor: 'github',
-  // 设置 showdown 默认 options （会覆盖上面 flavor 的 options）
-  options: {
-    emoji: false,
-  },
-})
+app
+  .use(VueShowdownPlugin, {
+    // 设置 showdown 默认 flavor
+    flavor: 'github',
+    // 设置 showdown 默认 options （会覆盖上面 flavor 的 options）
+    options: {
+      emoji: false,
+    },
+  })
+  .use(pinia)
 
 app.mount('#app')
