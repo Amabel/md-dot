db688740b32b55bf14213687d1bbbcfb8b5c5341
Amabel <luoweibinb@gmail.com>
2022-12-02 20:17:36 +0900

    feat: add style to textarea

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -16,14 +16,28 @@ import { VueShowdown } from 'vue-showdown'
 
 <style scoped lang="scss">
 .wrapper {
+  display: flex;
   margin-top: 24px;
   padding: 0 20px;
   height: 85%;
   width: 100vw;
 
   .textarea {
-    border: 1px solid #ccc;
+    flex: 1;
+    margin-right: 24px;
+    padding: 8px 12px;
+    min-width: 45%;
+    border-radius: 4px;
+    border: 1px solid var(--border);
+    background-color: var(--textarea-bg);
     resize: none;
+    outline: none;
+    transition: border-color 0.5s, background-color 0.5s;
+
+    &:focus {
+      background-color: var(--top-nav-bg);
+      box-shadow: 0 0 5px 2px var(--accent);
+    }
   }
 }
 </style>
